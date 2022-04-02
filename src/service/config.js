5fe2bb171e11ed17ab8ffec135c34e07875b03b0
Ulises Lopez Pacholczak <ulilopezpacho@gmail.com>
2022-04-02 09:56:09 -0300

    Easy commit

---
+++ src/service/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQULmT1-uliVgNfd8ELsh0kvU0JULPU1U",
+  authDomain: "grannygrannygo.firebaseapp.com",
+  projectId: "grannygrannygo",
+  storageBucket: "grannygrannygo.appspot.com",
+  messagingSenderId: "170392023829",
+  appId: "1:170392023829:web:fca3e289add0a830f8af1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
