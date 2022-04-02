1be0de147e082a78f920f62e9be6830c544ae0c9
Lucas Hernán Tarche <lucastarche@gmail.com>
2022-04-02 01:10:09 -0300

    Enabled compatibility with android

---
+++ lib/app.dart
@@ -0,0 +1,29 @@
+import 'dart:io'; // Add this import.
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+      ),
+    );
+  }
+}
